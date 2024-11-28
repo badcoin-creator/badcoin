8add30463fd0b50030976eb6bf51fa8c6d4ab393
BadCoin Creator <info@badcoin.com>
2024-11-28 11:06:34 +0100

    metadata + placeholder image

--- script/BadCoin.s.sol
+++ script/BadCoin.s.sol
@@ -6,7 +6,8 @@ import {BadCoin} from "../src/BadCoin.sol";
 
 contract BadCoinScript is Script {
     BadCoin public token;
-    string private uri = "https://test.com";
+    string private uri =
+        "https://raw.githubusercontent.com/badcoin-creator/badcoin/refs/heads/main/data/tokenMetadata.json";
 
     function setUp() public {}
 
